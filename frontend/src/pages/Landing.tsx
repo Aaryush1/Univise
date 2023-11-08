0f99cfc8e17bbe47821823eb24a5ad92dede1ebb
Harrison <hproloff@gmail.com>
2023-11-08 16:17:00 -0600

    added the icons and Features component

--- frontend/src/pages/Landing.tsx
+++ frontend/src/pages/Landing.tsx
@@ -3,12 +3,14 @@ import Container from '@mui/material/Container';
 import MainContent from '../components/LandingPage/MainContent';
 import Footer from '../components/shared/Footer';
 import OurStory from '../components/LandingPage/OurStory';
+import Features from '../components/LandingPage/Features';
 
 const Landing = () => {
   return (
     <Container maxWidth={false} disableGutters>
       <MainContent />
       <OurStory />
+      <Features />
       <Footer />
     </Container>
   );
