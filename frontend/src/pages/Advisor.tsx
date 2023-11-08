9b7e4a61f2feacbefd408dd2a94f30536f49b70c
Harrison <hproloff@gmail.com>
2023-11-08 14:02:13 -0600

    initial setup, npm install's and DOM structure

---
+++ frontend/src/pages/Advisor.tsx
@@ -0,0 +1,11 @@
+
+function Advisor() {
+    return (
+      <div>
+        <h1>Advisor Page</h1>
+        {/* Other components will go here */}
+      </div>
+    );
+  }
+  
+  export default Advisor;
---
+++ frontend/src/pages/Landing.tsx
@@ -0,0 +1,11 @@
+
+function Landing() {
+    return (
+      <div>
+        <h1>Landing Page</h1>
+        {/* Other components will go here */}
+      </div>
+    );
+  }
+  
+  export default Landing;
