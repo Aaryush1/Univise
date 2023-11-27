fb84bdd40c7ebfd31503a2f95adfcf1dd380c35f
Harrison <hproloff@gmail.com>
2023-11-27 16:33:36 -0600

    hardcode UX for demos on advisor/page.tsx

--- frontend/app/advisor/page.tsx
+++ frontend/app/advisor/page.tsx
@@ -14,10 +14,20 @@ export default function AdvisorPage() {
   const [currentQuestion, setCurrentQuestion] = useState('');
 
   const handleSendClick = () => {
-    const newHistory: ChatMessage[] = [
+    let responseText = '';
+
+    if (currentQuestion.includes('literature classes')) {
+      responseText = "Here are some recommendations based on your preferences:     ENG-104: Intro to Sci Fi: Exploring New Worlds, with average GPA of 3.78.     ENG-131: Contemporary Dystopian Visions, with average GPA of 3.66.     ENG-128: Science Fiction Short Stories, with average GPA of 3.81.      These courses all have highly-rated instructors and great grade point averages. Let me know if any of these interest you, or if you'd like more information or recommendations.";
+    } else if (currentQuestion.includes('lectures after noon')) {
+      responseText = "Yes, Intro to Sci Fi (ENG-104) has lectures on Monday and Wednesdays at 1:20pm. Also, Science Fiction Short Stories has lectures on Tuesdays and Thursdays at 2:30pm.";
+    } else {
+      responseText = "Sorry, I don't have information on that query.";
+    }
+
+    const newHistory = [
       ...chatHistory,
       { type: 'question', text: currentQuestion },
-      { type: 'response', text: `Here is the response to your question: ${currentQuestion}` },
+      { type: 'response', text: responseText },
     ];
     setChatHistory(newHistory);
     setCurrentQuestion(''); // Reset current question
