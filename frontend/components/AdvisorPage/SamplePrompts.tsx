4d902c0e43d9c092f362c18e4e6a9ce82d2512eb
aaryush1 <aaryush@outlook.com>
2024-04-02 11:43:33 -0500

    Sample Prompt update + engineering

--- frontend/components/AdvisorPage/SamplePrompts.tsx
+++ frontend/components/AdvisorPage/SamplePrompts.tsx
@@ -8,11 +8,11 @@ interface SamplePromptsProps {
 const SamplePrompts: React.FC<SamplePromptsProps> = ({ onSamplePrompt }) => {
   return (
     <Box style={{ display: 'flex', justifyContent: 'center', gap: '20px', padding: '20px' }}>
-      <Button style={{ width: '400px', borderRadius: '50px', padding: '8px 16px', textAlign: 'center', backgroundColor: 'white', border: '2px solid black', color: 'black', cursor: 'pointer', textTransform: 'capitalize' }} onClick={() => onSamplePrompt("What are some good literature electives?")}>
+      <Button style={{ width: '400px', borderRadius: '50px', padding: '8px 16px', textAlign: 'center', backgroundColor: 'white', border: '2px solid black', color: 'black', cursor: 'pointer', textTransform: 'capitalize' }} onClick={() => onSamplePrompt("What are some good literature electives I can take? Start by asking me questions to better understand what I'm looking for.")}>
         Easy literature electives?
       </Button>
-      <Button style={{ width: '400px', borderRadius: '50px', padding: '8px 16px', textAlign: 'center', backgroundColor: 'white', border: '2px solid black', color: 'black', cursor: 'pointer', textTransform: 'capitalize' }} onClick={() => onSamplePrompt("Help me plan for graduation")}>
-        Help me plan for graduation
+      <Button style={{ width: '400px', borderRadius: '50px', padding: '8px 16px', textAlign: 'center', backgroundColor: 'white', border: '2px solid black', color: 'black', cursor: 'pointer', textTransform: 'capitalize' }} onClick={() => onSamplePrompt("Help me find good classes for Computer Science. I'm a freshman that wants to take classes that will help me with my CS degree. What else do you need to know to find the best class for me?")}>
+        Freshman CS Classes?
       </Button>
     </Box>
   );
