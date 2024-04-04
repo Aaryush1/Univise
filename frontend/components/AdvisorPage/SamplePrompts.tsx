4865545e8b92517a0fc0db93ae5ccf6079dd51a8
aaryush1 <aaryush@outlook.com>
2024-04-03 21:18:30 -0500

    Prompt Update

--- frontend/components/AdvisorPage/SamplePrompts.tsx
+++ frontend/components/AdvisorPage/SamplePrompts.tsx
@@ -8,10 +8,10 @@ interface SamplePromptsProps {
 const SamplePrompts: React.FC<SamplePromptsProps> = ({ onSamplePrompt }) => {
   return (
     <Box style={{ display: 'flex', justifyContent: 'center', gap: '20px', padding: '20px' }}>
-      <Button style={{ width: '400px', borderRadius: '50px', padding: '8px 16px', textAlign: 'center', backgroundColor: 'white', border: '2px solid black', color: 'black', cursor: 'pointer', textTransform: 'capitalize' }} onClick={() => onSamplePrompt("What are some good literature electives I can take? Start by asking me questions to better understand what I'm looking for.")}>
+      <Button style={{ width: '400px', borderRadius: '50px', padding: '8px 16px', textAlign: 'center', backgroundColor: 'white', border: '2px solid black', color: 'black', cursor: 'pointer', textTransform: 'capitalize' }} onClick={() => onSamplePrompt("What are some good literature electives I can take? I want them to be super easy.")}>
         Easy literature electives?
       </Button>
-      <Button style={{ width: '400px', borderRadius: '50px', padding: '8px 16px', textAlign: 'center', backgroundColor: 'white', border: '2px solid black', color: 'black', cursor: 'pointer', textTransform: 'capitalize' }} onClick={() => onSamplePrompt("Help me find good classes for Computer Science. I'm a freshman that wants to take classes that will help me with my CS degree. What else do you need to know to find the best class for me?")}>
+      <Button style={{ width: '400px', borderRadius: '50px', padding: '8px 16px', textAlign: 'center', backgroundColor: 'white', border: '2px solid black', color: 'black', cursor: 'pointer', textTransform: 'capitalize' }} onClick={() => onSamplePrompt("Help me find good classes for Computer Science. I'm a freshman that wants to take classes that will help me with my CS degree.")}>
         Freshman CS Classes?
       </Button>
     </Box>
